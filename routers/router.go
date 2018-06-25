3ac57b56a7294aa6bcdc084983396f292bba0954
zaakin <305053045@qq.com>
2018-06-25 10:04:07 +0800

    modify entity bug

--- routers/router.go
+++ routers/router.go
@@ -4,9 +4,19 @@ import (
 	"idhub/saml_idhub_api/controllers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 func init() {
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+	}))
+
 	beego.Router("/setOrganizations/:id", &controllers.OrganizationController{})
 	beego.Router("/getMetadata/:id", &controllers.MetadataController{})
 	beego.Router("/getSamlResponse", &controllers.SamlResponseController{})
